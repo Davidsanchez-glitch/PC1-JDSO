e600858c4adce00fc92b974eb2a8499d5e60b11e
David <juansanchez@emilianisomascos.edu.gt>
2026-01-30 08:40:06 -0600

    Mi primer commit

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("Iniciando");
+        System.out.println("en crer");
+        System.out.println("repositorio");
+    }
+    
+}
